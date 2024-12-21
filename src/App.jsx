f6b0d5477c21b3a19cc7fe8987fb30e3bc801044
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-21 07:47:56 +0600

    react-app9

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Header from './Header.jsx';
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <Header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+            <li>Home</li>
+            <li>Project</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+      </nav>
+    </Header>
+  );
+}
+
+export default Header;
